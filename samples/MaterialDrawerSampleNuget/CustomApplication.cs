cffde3aa31cf1ded1d93e2e05c5de7717e5e5281
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-06-17 09:16:57 +0300

    [ADD] Added projects that tests created nuget package.

---
+++ samples/MaterialDrawerSampleNuget/CustomApplication.cs
@@ -0,0 +1,45 @@
+namespace MaterialDrawerSample
+{
+  using System;
+  using Android.App;
+  using Android.Content;
+  using Android.Graphics.Drawables;
+  using Android.Runtime;
+  using Android.Widget;
+  using Com.Mikepenz.Materialdrawer.Util;
+  using Squareup.Picasso;
+
+  [Application]
+  public class CustomApplication : Application
+  {
+
+    public CustomApplication(IntPtr handle, JniHandleOwnership ownerShip)
+      : base(handle, ownerShip)
+    {
+      DrawerImageLoader.Init(new DrawerImageLoaderImpl());
+    }
+
+    public override void OnCreate()
+    {
+      base.OnCreate();
+    }
+  }
+
+  class DrawerImageLoaderImpl : Java.Lang.Object, DrawerImageLoader.IDrawerImageLoader
+  {
+    public void Cancel(ImageView imageView)
+    {
+      Picasso.With(imageView.Context).CancelRequest(imageView);
+    }
+
+    public Drawable Placeholder(Context context)
+    {
+      return null;
+    }
+
+    public void Set(ImageView imageView, Android.Net.Uri uri, Drawable placeholder)
+    {
+      Picasso.With(imageView.Context).Load(uri).Placeholder(placeholder).Into(imageView);
+    }
+  }
+}
