5ffe4e9aea1ff95e8f1c24bfaa7c5b33852fad07
Andriy Matkivskiy <andriy.matkivskiy@gmail.com>
2015-12-17 14:46:01 +0200

    Updated NuGet package sample.

--- samples/MaterialDrawerSampleNuget/CustomApplication.cs
+++ samples/MaterialDrawerSampleNuget/CustomApplication.cs
@@ -1,13 +1,13 @@
+using Mikepenz.Iconics;
+using Sample;
+using Square.Picasso;
+
 namespace MaterialDrawerSample
 {
   using System;
   using Android.App;
-  using Android.Content;
-  using Android.Graphics.Drawables;
   using Android.Runtime;
-  using Android.Widget;
   using Mikepenz.MaterialDrawer.Utils;
-  using Square.Picasso;
 
   [Application]
   public class CustomApplication : Application
@@ -16,7 +16,7 @@ namespace MaterialDrawerSample
     public CustomApplication(IntPtr handle, JniHandleOwnership ownerShip)
       : base(handle, ownerShip)
     {
-      DrawerImageLoader.Init(new DrawerImageLoaderImpl());
+			DrawerImageLoader.Init(new AbstractDrawerImageLoaderImpl());
     }
 
     public override void OnCreate()
@@ -25,21 +25,35 @@ namespace MaterialDrawerSample
     }
   }
 
-  class DrawerImageLoaderImpl : Java.Lang.Object, DrawerImageLoader.IDrawerImageLoader
+	class AbstractDrawerImageLoaderImpl : AbstractDrawerImageLoader
   {
-    public void Cancel(ImageView imageView)
-    {
-      Picasso.With(imageView.Context).CancelRequest(imageView);
-    }
+		public override void Cancel(Android.Widget.ImageView imageView)
+		{
+			Picasso.With(imageView.Context).CancelRequest(imageView);
+		}
 
-    public Drawable Placeholder(Context context)
-    {
-      return null;
-    }
+		public override void Set(Android.Widget.ImageView imageView, Android.Net.Uri uri, Android.Graphics.Drawables.Drawable placeholder)
+		{
+			Picasso.With(imageView.Context).Load(uri).Placeholder(placeholder).Into(imageView);
+		}
 
-    public void Set(ImageView imageView, Android.Net.Uri uri, Drawable placeholder)
-    {
-      Picasso.With(imageView.Context).Load(uri).Placeholder(placeholder).Into(imageView);
-    }
+		public override Android.Graphics.Drawables.Drawable Placeholder(Android.Content.Context context, string tag)
+		{
+			//define different placeholders for different imageView targets
+			//default tags are accessible via the DrawerImageLoader.Tags
+			//custom ones can be checked via string. see the CustomUrlBasePrimaryDrawerItem LINE 111
+			if (DrawerImageLoader.Tags.Profile.Name() == tag) {
+				return DrawerUIUtils.GetPlaceHolder(context);
+			} else if (DrawerImageLoader.Tags.AccountHeader.Name() == tag) {
+				return new IconicsDrawable(context).IconText(" ").BackgroundColorRes(Resource.Color.primary).SizeDp(56);
+			} else if ("customUrlItem" == tag) {
+				return new IconicsDrawable(context).IconText(" ").BackgroundColorRes(Resource.Color.md_red_500).SizeDp(56);
+			}
+
+			//we use the default one for
+			//DrawerImageLoader.Tags.PROFILE_DRAWER_ITEM.name()
+
+			return base.Placeholder(context, tag);
+		}
   }
 }
